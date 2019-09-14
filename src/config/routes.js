ea836c5108c5f841199c40d1f4aa764c285d5379
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2019-09-14 16:56:23 -0300

    Inserindo autenticação no backend usando jwt

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const auth = require("./auth");
 
 module.exports = server => {
-    // Definir url base para todas as rotas
-    const router = express.Router();
-    server.use("/api", router);
-
-    // Definir rotas do ciclo de pagamento
-    const billingCycle = require("../api/billingCycle/billingCycleService");
-    billingCycle.register(router, "/billingCycles");
+    /*
+     * Rotas protegidas por Token JWT
+     */
+    const protectedApi = express.Router();
+    server.use("/api", protectedApi);
+    protectedApi.use(auth);
+    const BillingCycle = require("../api/billingCycle/billingCycleService");
+    BillingCycle.register(protectedApi, "/billingCycles");
+    /*
+     * Rotas abertas
+     */
+    const openApi = express.Router();
+    server.use("/oapi", openApi);
+    const AuthService = require("../api/user/authService");
+    openApi.post("/login", AuthService.login);
+    openApi.post("/signup", AuthService.signup);
+    openApi.post("/validateToken", AuthService.validateToken);
 };
